23f8b89626ba7fa9344df1c86f8abcf84a91bfe1
Will Moss <wmoss@bu.mp>
2013-05-23 21:39:40 -0700

    A start down the road of switching over to Dart
    I've stripped out all the dynamic code from the main page and will slowly be trying to add it back

---
+++ dart/build_dart.sh
@@ -0,0 +1,2 @@
+dart --package-root=packages/ packages/web_ui/dwc.dart --out=out --package-root=packages/ web/main.html
+dart2js --out=out/main.html_bootstrap.dart.js --package-root=packages/ out/main.html_bootstrap.dart
---
+++ dart/web/main.dart
@@ -0,0 +1,11 @@
+import 'dart:html';
+import 'dart:async';
+import 'package:web_ui/web_ui.dart';
+
+@observable
+String userName = 'test name';
+
+void main() {
+  // Enable this to use Shadow DOM in the browser.
+  //useShadowDom = true;
+}
