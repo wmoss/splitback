73c72b2975d759fe11273728ba888454dc09cc3c
Will Moss <wmoss@bu.mp>
2013-05-25 10:38:46 -0700

    Move the build directory to web/out so that it can work with the Dart editor's automatic build

--- dart/build_dart.sh
+++ dart/build_dart.sh
@@ -1,3 +1,2 @@
-dart --package-root=packages/ packages/web_ui/dwc.dart --out=out --package-root=packages/ --basedir=./ web/main.html
-mv out/web/* out/
-dart2js --out=out/main.html_bootstrap.dart.js --package-root=packages/ out/main.html_bootstrap.dart
+dart --package-root=packages/ packages/web_ui/dwc.dart --out=web/out --package-root=packages/ --basedir=./ web/main.html
+dart2js --out=web/out/web/main.html_bootstrap.dart.js --package-root=packages/ web/out/web/main.html_bootstrap.dart
