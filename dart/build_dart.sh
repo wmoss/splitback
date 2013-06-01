b7ce07cba8613d611ef20a99a57e1cf19b38d105
Will Moss <wmoss@bu.mp>
2013-05-31 23:04:02 -0700

    Update dart js build script to work when run from any directory

--- dart/build_dart.sh
+++
@@ -1,2 +0,0 @@
-dart --package-root=packages/ packages/web_ui/dwc.dart --out=web/out --package-root=packages/ --basedir=./ web/main.html
-dart2js --out=web/out/web/main.html_bootstrap.dart.js --package-root=packages/ web/out/web/main.html_bootstrap.dart
